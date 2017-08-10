d39aa614a799829420ee6ddc56acaf4ba33788a5
Juga Paazmaya <paazmaya@yahoo.com>
2017-08-10 21:12:46 +0300

    How about some tests?

--- tests/lib/handle-groups_test.js
+++ tests/lib/handle-groups_test.js
@@ -12,3 +12,37 @@ const path = require('path');
 
 const tape = require('tape'),
   handleGroups = require('../../lib/handle-groups');
+
+tape('handleGroups does nothing when groups is empty', (test) => {
+  test.plan(1);
+
+  const directory = __dirname;
+  const groups = {};
+  const options = {
+    lowercaseSuffix: false,
+    verbose: false,
+    dryRun: false
+  };
+
+  const result = handleGroups(directory, groups, options);
+
+  test.equal(result, 0);
+});
+
+tape('handleGroups increments when group is not existing and dry running', (test) => {
+  test.plan(1);
+
+  const directory = __dirname;
+  const groups = {
+    'does-not-exist': ['not-here.txt']
+  };
+  const options = {
+    lowercaseSuffix: false,
+    verbose: false,
+    dryRun: true
+  };
+
+  const result = handleGroups(directory, groups, options);
+
+  test.equal(result, 1);
+});
