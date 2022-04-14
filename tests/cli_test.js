821b67003f0af4d05882beadbfe042698b70dd7a
Juga Paazmaya <paazmaya@yahoo.com>
2022-04-14 16:41:54 +0300

    ESLint plugin for Node.js in use

--- tests/cli_test.js
+++ tests/cli_test.js
@@ -8,7 +8,9 @@
 
 const fs = require('fs'),
   path = require('path'),
-  execFile = require('child_process').execFile;
+  {
+    execFile
+  } = require('child_process');
 
 const tape = require('tape');
 
